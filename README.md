62c0cf9bd2a7418541a64786dabb6b82e2c13a08
wkev <speedo.copp@gmail.com>
2016-10-30 12:07:28 -0400

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# porno [![npm version](https://badge.fury.io/js/porno.svg)](https://badge.fury.io/js/porno)
+(to view parameters for each function, refer to the [API Page](http://api.porn.com)) 
+## Main Functions
+
+### videoSearch
---
+++ src/index.js
@@ -0,0 +1,45 @@
+var superagent = require('superagent')
+
+var Porno = module.exports = {}; // fun time
+
+  Porno.apiRequest = (path, options = {}) => {
+   let params = "";
+   for(let opt in options){
+  params += `&${opt}=${options[opt]}`
+ };
+    return new Promise((resolve, reject) => {
+      superagent.get(`http://api.porn.com/${path}${params}`)
+      .end((err, res) => {
+        if (err) return reject(err);
+        return resolve(res.body);
+      });
+    })
+ };
+ 
+ Porno.videoSearch = (options = {}) => {
+  return Porno.apiRequest('videos/find.json', options);
+};
+
+ Porno.videoIsActive =  (id = 0) => {
+  return Porno.apiRequest('videos/is-active.json', {id: id});
+};
+
+Porno.fetchDeletedVideos = (options = {}) => {
+  return Porno.apiRequest('videos/deleted.json', options);
+};
+
+Porno.actorSearch = (options = {}) => {
+  return Porno.apiRequest('actors/find.json', options);
+};
+
+Porno.channelSearch = (options = {}) => {
+  return Porno.apiRequest('channels/find.json', options);
+};
+
+Porno.dvdSearch = (options = {}) => {
+  return Porno.apiRequest('dvds/find.json', options);
+};
+
+Porno.fetchCategories =  () => {
+  return Porno.apiRequest('categories/find.json');
+};
